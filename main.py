ca44e4760395a64d4ee21bdcf2f84772f8ca2b8a
LUIS.GUAYACAN@correo.uis.edu.co <luis.guayacan@correo.uis.edu.co>
2020-12-11 16:29:44 -0500

    Ver por Tipo (egreso/ingreso)

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ async def obtener_balance(id_usuario : int):
 
     return {"data":lista_final, "balance":sum(ingresos)-sum(egresos)}
 
+
 @app.post("/registro/")
 async def crear_registro(registro: db.Registro):
     creado_exitosamente = db.crear_registro(registro)
@@ -28,3 +29,20 @@ async def crear_registro(registro: db.Registro):
         return {"mensaje: Registro creado exitosamente"}
     else:
         raise HTTPException(status_code=400, detail="Error, la ID ya se encuentra registrada")
+
+
+@app.get("/registro/tipo/{id_usuario}")
+async def obtener_balance(id_usuario : int,Tipo : str):
+    registros = db.obtener_registros()
+    lista_usuario = []
+    tipo_registro = []
+    
+    for i in range(len(registros)):
+        if registros[i].id_usuario == id_usuario:
+           lista_usuario.append(registros[i])
+    
+    for i in range(len(lista_usuario)):
+        if lista_usuario[i].tipo == Tipo:
+            tipo_registro.append(lista_usuario[i])
+
+    return {Tipo:tipo_registro}
