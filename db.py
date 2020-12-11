256d821bb8c2e23a8454f4a9a779e3b4cef8a762
Luis-Sarmiento <luissarmientomarquez@gmail.com>
2020-12-11 13:15:38 -0500

    Endpoint crear registro

--- db.py
+++ db.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel
+#from datetime import datetime
 
 class Registro(BaseModel):
-    id_usuario: int
     id: int
+    id_usuario: int
     tipo: str
     valor: int
-    fecha: str
+    fecha: str 
     etiqueta: str
     nota: str
 
 registros = {
-  1: Registro(id_usuario=1, id=1, tipo="egreso", valor= 2600, fecha= "10-12-20", etiqueta="comida", nota="pina"),
-  2: Registro(id_usuario=1, id=2, tipo="ingreso", valor=28000, fecha="10-12-20", etiqueta="venta", nota="galletas y torta"),
-  3: Registro(id_usuario=2, id=1, tipo="egreso", valor=2600, fecha="10-12-20", etiqueta="comida", nota="pina"),
-  4: Registro(id_usuario=2, id=2, tipo="ingreso", valor=28000, fecha="10-12-20", etiqueta="venta", nota="galletas y torta")
+  1: Registro(id=1, id_usuario=1, tipo="egreso", valor= 2600, fecha="10-12-2020", etiqueta="comida", nota="pina"),
+  2: Registro(id=2, id_usuario=1, tipo="ingreso", valor=28000, fecha="10-12-2020", etiqueta="venta", nota="galletas y torta"),
+  3: Registro(id=3, id_usuario=2, tipo="egreso", valor=2600, fecha="10-12-2020", etiqueta="comida", nota="pina"),
+  4: Registro(id=4, id_usuario=2, tipo="ingreso", valor=28000, fecha="10-12-2020", etiqueta="venta", nota="galletas y torta")
 }
 
 def obtener_registros():
@@ -22,3 +23,10 @@ def obtener_registros():
       lista_datos.append(registros[e])
         #for i in range lista_datos.len():
     return lista_datos
+
+def crear_registro(registro: Registro):
+      if registro.id in registros:
+            return False
+      else:
+            registros[registro.id] = registro
+            return True 
--- main.py
+++ main.py
@@ -11,3 +11,11 @@ async def obtener_balance():
         if registros[i].id_usuario == 1:
            lista_final.append(registros[i])
     return lista_final
+
+@app.post("/registro/")
+async def crear_registro(registro: db.Registro):
+    creado_exitosamente = db.crear_registro(registro)
+    if creado_exitosamente:
+        return {"mensaje: Registro creado exitosamente"}
+    else:
+        raise HTTPException(status_code=400, detail="Error, la ID ya se encuentra registrada")
